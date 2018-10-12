d7ed6be5ea3a937c4ec653f8061f0f16b6d47236
JJ Ashcraft <jj@jjashcraft.com>
2018-10-12 13:58:16 -0400

    ex1.py complete

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -1,6 +1,25 @@
 def get_indices_of_item_weights(weights, limit):
-  pass 
+  weight_table = {}
+  result = ()
+  if len(weights) == 2:
+    if (weights[0] + weights[1] == limit):
+      print((1,0))
+      return (1,0)
+  
+  for index, w in enumerate(weights):
+    weight_table[w] = index #assigns key to weight, value to list index
+  for k,v in weight_table.items(): #prints list of key/value pairs
+    print(k, ':', v)
+    if ((limit - weights[v]) in weight_table):
+      print('success')
+      result = result + (v,)
+  print(tuple(sorted(result,reverse=True)))    
+  return tuple(sorted(result,reverse=True))
+
+
 
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
-  pass 
+  # get_indices_of_item_weights([4, 6, 10, 15, 16], 21)
+  get_indices_of_item_weights([4, 4], 8)
+
