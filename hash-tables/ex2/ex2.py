8b14b64fb4ba4507ddf6eb87714992f5b34fe319
JJ Ashcraft <jj@jjashcraft.com>
2018-10-12 14:26:34 -0400

    ticket_table (hash table) created for test data set, in progress

--- hash-tables/ex2/ex2.py
+++ hash-tables/ex2/ex2.py
@@ -1,6 +1,39 @@
 def reconstruct_trip(tickets):
-  pass 
+  ticket_table = {}
+  for ticket in tickets:
+    ticket_table[ticket[0]] = ticket[1]
+  print(ticket_table)
 
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
-  pass
+  short_set = [
+      (None, 'PDX'),
+      ('PDX', 'DCA'),
+      ('DCA', None)
+    ]
+  reconstruct_trip(short_set)
+
+# class Node(object):
+
+#     def __init__(self, data=None, next_node=None):
+#         self.data = data
+#         self.next_node = next_node
+
+#     def get_data(self):
+#         return self.data
+
+#     def get_next(self):
+#         return self.next_node
+
+#     def set_next(self, new_next):
+#         self.next_node = new_next
+        
+# class LinkedList(object):
+#     def __init__(self, head=None):
+#         self.head = head
+#     def insert(self, data):
+#       new_node = Node(data)
+#       new_node.set_next(self.head)
+#       self.head = new_node
+#     def all(self):
+      
